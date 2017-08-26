a1434971af15928234224270d903704d98ecf983
jamesmckeon <jbmckeon@comcast.net>
2017-08-25 18:00:57 -0700

    some css changes

--- GeoHub.Services/Global.asax.cs
+++ GeoHub.Services/Global.asax.cs
@@ -7,12 +7,13 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using GeoHub.Services;
+using GeoHub.Services.Logging;
 
 namespace GeoHub.Services
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-     
+     protected IAppLogger Logger = new NlogLogger();
         protected void Application_Start()
         {
 
@@ -21,6 +22,13 @@ namespace GeoHub.Services
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             AreaRegistration.RegisterAllAreas();
 
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        
+            Exception ex = Server.GetLastError();
+            Logger.Error("UnhandledExceptions", ex);
         }
     }
 }
